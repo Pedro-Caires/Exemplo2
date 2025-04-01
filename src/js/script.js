16aeb9838206bb532d59dd5b9aa23943e55ebd20
Pedrinho <pedrohc2105@gmail.com>
2025-04-01 09:17:14 -0300

    Adicionando tipos de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+// declarações e variaveis
+var nome ="fiap";
+console.log(nome)
+
+let idade =17;
+console.log(idade)
+
+const sobrenome ="Caires"
+console.log(sobrenome)
+// undefined
+let aula; 
+console.log(aula)
+// null
+let valor = null;
+console.log(valor)
+
+let exemplo1={}; 
+
+let exemplo2=[];
+
+// TIPO VARIAVEIS
+let exemplo3 = 10;
+console.log(typeof exemplo3)
+let exemplo4 = "aula"
+console.log(typeof exemplo4)
+
+let exemplo5 =true;
+console.log(typeof exemplo5)
+
+let exemplo6 =["pedrinho", "dioguinho", "lorenzinho"];
+console.log(typeof exemplo6)
+
+// CONVERSÕES
