5b52123b6791a3007e666e6dce71479bf6d14c68
Pedro-Caires <pedrohc2105@gmail.com>
2025-04-08 09:37:54 -0300

    adicionando o sweat case e a estrutura de repetição

--- src/js/script.js
+++ src/js/script.js
@@ -121,7 +121,54 @@
 
 // Condição ternaria
 
-let valor2 =301;
+// let valor2 =301;
 
-let resultado = valor2 == 300 ? "certo": "errado"
-console.log (resultado)
+// let resultado = valor2 == 300 ? "certo": "errado"
+// console.log (resultado)
+
+// SWITCH CASE
+
+// let time ="Corinthians";
+
+// switch(time){
+//     case "Corinthians":
+//         console.log("Melhor Time")
+//         break;
+//     case "Palmeiras":
+//         console.log("Não tem um mundial")
+//         break;
+//     case "São Paulo":
+//         console.log("time de viado")
+//         break;
+
+// }
+
+// // ESTRUTURAS DE LAÇOS
+
+// for(let i=0;i<10;i++){
+//     console.log("O valor de I é:", i)
+// }
+
+
+
+// ESTRUTURA DE LAÇO DE REPETIÇÃO
+
+// WHILE
+
+let w=0
+
+while(w <=10){
+    console.log("o valor de W é",w)
+    w++;
+}
+
+// do while
+
+let z=10;
+
+do{
+    console.log("do while é",z)
+    z++;
+}while(z<=100)
+
+    
